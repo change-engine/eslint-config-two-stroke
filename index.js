429396a7f654e1bdd4023a6b3c654e6e4f9d6f9e
Barnaby Shearer <barnaby@changeengine.com>
2026-03-18 11:47:54 +0000

    omit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ export default [
     files: ["**/*.ts", "**/*.tsx"],
   })),
   {
-    ignores: ["dist", ".wrangler", "**/api.d.ts", "**/*-definitions.ts"],
+    ignores: ["worker-configuration.d.ts", "dist", ".wrangler", "**/api.d.ts", "**/*-definitions.ts"],
   },
   {
     files: ["**/*.ts", "**/*.tsx"],
